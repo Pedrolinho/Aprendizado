2a648acd74b9e5552eb19f8a25589f1a706354e4
Pedrolinho <pedroandre.lcc@gmail.com>
2022-01-14 09:48:31 -0300

    New function in Módulo

--- Aprendizado/MóduloPessoal.py
+++ Aprendizado/MóduloPessoal.py
@@ -1,2 +1,19 @@
 def somar(x, y):
     print(x + y)
+
+
+def IMC(altura, peso):
+
+    indice = peso / ((altura / 100)** 2)
+    print(f'O seu índice IMC é: {indice}')
+
+    if indice >= 40:
+        print('O estado é de obesidade grave.')
+    elif indice >= 30:
+        print('O estado é de obesidade.')
+    elif indice >= 25:
+        print('O estado é de sobrepeso.')
+    elif indice >= 18.5:
+        print('O estado é normal.')
+    else:
+        print('O estado é de magreza.')
